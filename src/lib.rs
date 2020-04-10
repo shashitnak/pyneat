20c5590a8364003614ec51a1f15104b1dc9663a2
Shashi Kant <shashitnak@gmail.com>
2020-04-10 13:17:59 +0530

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use pyo3::prelude::*;
+use neat_rs::Genotype;
+
+#[pyclass]
+struct Network {
+    net: slow_nn::Network
+}
+
+fn sigmoid(x: f64) -> f64 {
+    1. / (1. + (-x).exp())
+}
+
+fn tanh(x: f64) -> f64 {
+    x.tanh()
+}
+
+#[pymethods]
+impl Network {
+    fn predict(&self, input: Vec<f64>, activation: String) -> Vec<f64> {
+        if activation == "sigmoid" {
+            self.net.predict(&input, sigmoid)
+        } else {
+            self.net.predict(&input, tanh)
+        }
+    }
+}
+
+#[pyclass]
+struct Neat {
+    neat: neat_rs::Neat<Genotype>
+}
+
+#[pymethods]
+impl Neat {
+    #[new]
+    fn new(inputs: usize, outputs: usize, size: usize, mutation_rate: f64) -> Self {
+        Self {
+            neat: neat_rs::Neat::new(inputs, outputs, size, mutation_rate)
+        }
+    }
+
+    fn eval(&mut self, func: PyObject) {
+        self.neat.next_generation(|genome, display| {
+            let gil = Python::acquire_gil();
+            let py = gil.python();
+            let network = Network { net: genome.get_network() };
+            let score: f64 = func
+                .call1(py, (network, display))
+                .expect("Error occured when calling function")
+                .extract(py)
+                .expect("Could not convert to float");
+            score
+        });
+    }
+}
+
+#[pymodule]
+fn neat_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Neat>()?;
+    Ok(())
+}
